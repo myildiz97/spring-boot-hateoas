88e09d7dcb166acaab6c4150e37f221939b607c4
Mehmet Yıldız <yildiz.mehmet@metu.edu.tr>
2024-05-02 11:16:15 +0300

    add post put and delete methods

--- src/main/java/com/ims/resthateoas/StudentController.java
+++ src/main/java/com/ims/resthateoas/StudentController.java
@@ -9,8 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,5 +37,36 @@ public class StudentController {
     Student student = studentRepository.findById(id);
     student.add(linkTo(methodOn(StudentController.class).getStudent(id)).withSelfRel());
     return new HttpEntity<>(student);
+  }
+
+  @PostMapping("/students")
+  public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+    Student newStudent = new Student(student.getFirstName(), student.getLastName());
+    studentRepository.save(newStudent);
+    student.add(linkTo(methodOn(StudentController.class).getStudent(student.getStudentId())).withSelfRel());
+    return new ResponseEntity<>(newStudent, HttpStatus.CREATED);
+  }
+
+  @PutMapping("/students/{id}")
+  public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student student) {
+    Student existingStudent = studentRepository.findById(id);
+    if (existingStudent == null) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    existingStudent.setFirstName(student.getFirstName());
+    existingStudent.setLastName(student.getLastName());
+    studentRepository.save(existingStudent);
+    existingStudent.add(linkTo(methodOn(StudentController.class).getStudent(id)).withSelfRel());
+    return new ResponseEntity<>(existingStudent, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/students/{id}")
+  public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
+    Student student = studentRepository.findById(id);
+    if (student == null) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    studentRepository.deleteById(id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 }
